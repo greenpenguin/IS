8d5490f6f3f2703393b78303f94e059644c516f3
greenpenguin <talalaykinaei@gmail.com>
2019-10-20 00:05:22 +0400

    no message

---
+++ Task6/EncryptionClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task6
+{
+    public class EncryptionClass
+    {
+        private string MakeKeywordLengthEqualToMessageLength(int messageLength, string keyWord)
+        {
+            int lengthDifference = messageLength / keyWord.Length;
+                        
+            StringBuilder keyWordStringBuilder = new StringBuilder();
+
+            for (int i = 0; i < lengthDifference; i++)
+            {
+                keyWordStringBuilder.Append(keyWord);
+            }
+            foreach (var elem in keyWord)
+            {
+                if (keyWordStringBuilder.Length != messageLength)
+                {
+                    keyWordStringBuilder.Append(elem);
+                    
+                }
+            }
+
+            return keyWordStringBuilder.ToString();
+        }
+        
+        public List<char> Encryption(string messageForEncryption, string keyWord, char[,] vigenereTableInMatrix)
+        {
+            List<char> codeOfMessage = new List<char>();
+
+            keyWord = MakeKeywordLengthEqualToMessageLength(messageForEncryption.Length, keyWord);
+            for (int i = 0; i < messageForEncryption.Length; i++)
+            {
+                for (int m = 0; m < vigenereTableInMatrix.GetLength(0); m++)
+                {
+                    for (int k = 0; k < vigenereTableInMatrix.GetLength(1); k++)
+                    {
+                        if ((messageForEncryption[i] == vigenereTableInMatrix[m, 0]) &&
+                            (keyWord[i] == vigenereTableInMatrix[0, k]))
+                        {
+                            codeOfMessage.Add(vigenereTableInMatrix[m, k]);
+                        }
+                    }
+                }
+            }
+
+            return codeOfMessage;
+        }
+
+        public StringBuilder Decryption(List<char> messageForDecryption, string keyWord, char[,] vigenereTableInMatrix)
+        {
+            StringBuilder result = new StringBuilder();
+            
+            keyWord = MakeKeywordLengthEqualToMessageLength(messageForDecryption.Count, keyWord);
+            
+            for (int i = 0; i < messageForDecryption.Count; i++)
+            {
+                for (int m = 0; m < vigenereTableInMatrix.GetLength(0); m++)
+                {
+                    for (int k = 0; k < vigenereTableInMatrix.GetLength(1); k++)
+                    {
+                        if ((messageForDecryption[i] == vigenereTableInMatrix[m, k]) &&
+                            (keyWord[i] == vigenereTableInMatrix[0, k]))
+                        {
+                            result.Append(vigenereTableInMatrix[m, 0]);
+                        }
+                    }
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ Task6/WorkWithFileClass.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Task6
+{
+    public class WorkWithFileClass
+    {
+        public string ReadFile()
+        {
+            string inputFilePath = Directory.GetCurrentDirectory() + "/input.txt";
+
+            using (StreamReader reader = new StreamReader(inputFilePath, Encoding.Default))
+            {
+                string outputStr = reader.ReadToEnd();
+                StringBuilder outputStringBuilder = new StringBuilder();
+
+                foreach (var elem in outputStr)
+                {
+                    if (IsRusLetter(elem))
+                    {
+                        outputStringBuilder.Append(char.ToUpper(elem));
+                    }
+                }
+                return outputStringBuilder.ToString();
+            }
+        }
+        
+        private bool IsRusLetter(char letter)
+        {
+            if ((letter > 'А') && (letter < 'я'))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public char[,] CreateVigenereTableFromFile()
+        {
+            string vigenereTableFilePath = Directory.GetCurrentDirectory() + "/VigenereTable.txt";
+            char[,] vigenereTableInMatrix = new char[33, 33];
+            using (StreamReader reader = new StreamReader(vigenereTableFilePath, Encoding.Default))
+            {
+                for (int i = 0; i < vigenereTableInMatrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < vigenereTableInMatrix.GetLength(1); j++)
+                    {
+                        vigenereTableInMatrix[i, j] = (char) reader.Read();
+                    }
+                }
+                
+                
+            }
+            
+            /*for (int i = 0; i < vigenereTableInMatrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < vigenereTableInMatrix.GetLength(1); j++)
+                {
+                    Console.Write(vigenereTableInMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }*/
+            
+            return vigenereTableInMatrix;
+        }
+
+        public void OutputResultToFile(List<char> encryptedMessage, StringBuilder decryptedMessage)
+        {
+            string outputFilePath = Directory.GetCurrentDirectory() + "/output.txt";
+
+            File.WriteAllText(outputFilePath, String.Empty);
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                writer.WriteLine("Encrypted message:");
+                foreach (var elem in encryptedMessage)
+                {
+                    writer.Write(elem + " ");
+                }
+
+                writer.WriteLine();
+                string decryptedMessageForOutput = decryptedMessage.ToString();
+                writer.WriteLine("Decrypted message:");
+                foreach (var elem in decryptedMessageForOutput)
+                {
+                    writer.Write(elem);
+                }
+            }
+
+        }
+    }
+}
