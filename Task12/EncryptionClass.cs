98688be5c127498d258c911c4be3147721bd266a
greenpenguin <talalaykinaei@gmail.com>
2019-10-23 11:32:14 +0400

    no message

---
+++ Task12/EncryptionClass.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task12
+{
+    public class EncryptionClass
+    {
+        private char Xor(char firstBit, char secondBit)
+        {
+            if (((firstBit == '0') && (secondBit == '0')) || ((firstBit == '1') && (secondBit == '1')))
+            {
+                return '0';
+            }
+            else
+            {
+                return '1';
+            }
+        }
+        
+        public List<string> Encryption(string messageForEncryption, string keyWord, Dictionary<int, string> binaryCodeOfAlphabet, 
+            Dictionary<int, char> decimalCodeAndLetterOfAlphabet)
+        {
+            List<string> binaryCodeOfMessage = new List<string>();
+            foreach (var elem in messageForEncryption)
+            {
+                int elemId = -1;
+                foreach (var decimalCode in decimalCodeAndLetterOfAlphabet)
+                {
+                    if (decimalCode.Value == elem)
+                    {
+                        elemId = decimalCode.Key;
+                    }
+                }
+
+                foreach (var binaryCode in binaryCodeOfAlphabet)
+                {
+                    if (binaryCode.Key == elemId)
+                    {
+                        binaryCodeOfMessage.Add(binaryCode.Value);
+                    }
+                }
+            }
+            
+            StringBuilder binaryCodeOfMessageInOneLine = new StringBuilder();
+            foreach (var elem in binaryCodeOfMessage)
+            {
+                binaryCodeOfMessageInOneLine.Append(elem);
+            }
+            
+            RegisterClass registerClass = new RegisterClass();
+            var binaryCodeOfKeyWord = registerClass.Register(keyWord, binaryCodeOfMessageInOneLine.Length, binaryCodeOfAlphabet,
+                decimalCodeAndLetterOfAlphabet);
+            
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < binaryCodeOfMessage.Count; i++)
+            {
+                StringBuilder strForAdding = new StringBuilder();
+                for (int j = 0; j < binaryCodeOfMessage[i].Length; j++)
+                {
+                    strForAdding.Append(Xor(binaryCodeOfMessage[i][j], binaryCodeOfKeyWord[i]));
+                }
+                
+                result.Add(strForAdding.ToString());
+            }
+            
+            return result;
+        }
+
+        public StringBuilder Decryption(List<string> messageForDecryption, string keyWord, Dictionary<int, string> binaryCodeOfAlphabet, 
+            Dictionary<int, char> decimalCodeAndLetterOfAlphabet)
+        {
+            StringBuilder binaryCodeOfMessageInOneLine = new StringBuilder();
+            foreach (var elem in messageForDecryption)
+            {
+                binaryCodeOfMessageInOneLine.Append(elem);
+            }
+            
+            RegisterClass registerClass = new RegisterClass();
+            var binaryCodeOfKeyWord = registerClass.Register(keyWord, binaryCodeOfMessageInOneLine.Length, binaryCodeOfAlphabet,
+                decimalCodeAndLetterOfAlphabet);
+            
+            List<string> codeOfMessage = new List<string>();
+
+            for (int i = 0; i < messageForDecryption.Count; i++)
+            {
+                StringBuilder strForAdding = new StringBuilder();
+                for (int j = 0; j < messageForDecryption[i].Length; j++)
+                {
+                    strForAdding.Append(Xor(messageForDecryption[i][j], binaryCodeOfKeyWord[i]));
+                }
+                
+                codeOfMessage.Add(strForAdding.ToString());
+            }
+            
+            StringBuilder result = new StringBuilder();
+
+            foreach (var elem in codeOfMessage)
+            {
+                int elemId = -1;
+                
+                foreach (var binaryCode in binaryCodeOfAlphabet)
+                {
+                    if (binaryCode.Value == elem)
+                    {
+                        elemId = binaryCode.Key;
+                    }
+                }
+                
+                foreach (var decimalCode in decimalCodeAndLetterOfAlphabet)
+                {
+                    if (decimalCode.Key == elemId)
+                    {
+                        result.Append(decimalCode.Value);
+                    }
+                }
+            }
+            
+            return result;
+        }
+    }
+}
---
+++ Task12/RegisterClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task12
+{
+    public class RegisterClass
+    {
+        private char Xor(char firstBit, char secondBit)
+        {
+            if (((firstBit == '0') && (secondBit == '0')) || ((firstBit == '1') && (secondBit == '1')))
+            {
+                return '0';
+            }
+            else
+            {
+                return '1';
+            }
+        }
+
+        public string Register(string inputString, int lengthOfMessage, Dictionary<int, string> binaryCodeOfAlphabet, 
+            Dictionary<int, char> decimalCodeAndLetterOfAlphabet)
+        {
+            // перевод введенного ключа в двоичный код
+            List<string> binaryCodeOfInputString = new List<string>();
+            foreach (var elem in inputString)
+            {
+                int elemId = -1;
+                foreach (var decimalCode in decimalCodeAndLetterOfAlphabet)
+                {
+                    if (decimalCode.Value == elem)
+                    {
+                        elemId = decimalCode.Key;
+                    }
+                }
+
+                foreach (var binaryCode in binaryCodeOfAlphabet)
+                {
+                    if (binaryCode.Key == elemId)
+                    {
+                        binaryCodeOfInputString.Add(binaryCode.Value);
+                    }
+                }
+            }
+
+            /*foreach (var elem in binaryCodeOfInputString)
+            {
+                Console.Write(elem);
+            }
+            Console.WriteLine();*/
+            
+            // создание единой строки из полученного листа
+            StringBuilder outputStr = new StringBuilder();
+
+            foreach (var elem in binaryCodeOfInputString)
+            {
+                outputStr.Append(elem);
+            }
+
+            // пока длина генерируемой строки меньше длины сообщения, которое будем шифровать,
+            // производим регистровый сдвиг
+            for (int i = 0; i < outputStr.ToString().Length; i++)
+            {
+                if (outputStr.ToString().Length < lengthOfMessage)
+                {
+                    outputStr.Append(Xor(outputStr.ToString()[i], 
+                        outputStr.ToString()[outputStr.ToString().Length - 1]));
+                }
+            }
+            /*foreach (var elem in outputStr.ToString())
+            {
+                Console.Write(elem);
+            }*/
+
+            return outputStr.ToString();
+        }
+    }
+}
